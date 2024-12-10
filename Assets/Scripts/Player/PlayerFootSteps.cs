4c35100577e65a49d8ddff4cd078a02fbad87193
mervezgin <mervezgin@gmail.com>
2024-12-10 10:52:32 +0300

    added player footstep sound when player walking, sprinting and crouching

--- Assets/Scripts/Player/PlayerFootSteps.cs
+++ Assets/Scripts/Player/PlayerFootSteps.cs
@@ -2,17 +2,43 @@ using UnityEngine;
 
 public class PlayerFootSteps : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] footStepClip;
+    private CharacterController characterController;
     private AudioSource footStepSound;
-    private AudioClip[] footStepClip;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [HideInInspector] public float volumeMin, volumeMax;
+    [HideInInspector] public float stepDistance;
+    private float accumulatedDistance; //how far can we go before play sound 
+    private void Awake()
     {
-
+        footStepSound = GetComponent<AudioSource>();
+        characterController = GetComponentInParent<CharacterController>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        CheckToPlayFootstepSound();
+    }
+    private void CheckToPlayFootstepSound()
+    {
+        if (!characterController.isGrounded) return;
+        if (characterController.velocity.sqrMagnitude > 0)
+        {
+            //accumulatedDistance is the value how far can we go 
+            //make a step or sprint or move while crouching
+            //until we play the footstepSound
+            accumulatedDistance += Time.deltaTime;
 
+            if (accumulatedDistance > stepDistance)
+            {
+                footStepSound.volume = Random.Range(volumeMin, volumeMax);
+                footStepSound.clip = footStepClip[Random.Range(0, footStepClip.Length)];
+                footStepSound.Play();
+
+                accumulatedDistance = 0;
+            }
+        }
+        else
+        {
+            accumulatedDistance = 0;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerSprintAndCrouch.cs
+++ Assets/Scripts/Player/PlayerSprintAndCrouch.cs
@@ -3,17 +3,31 @@ using UnityEngine;
 public class PlayerSprintAndCrouch : MonoBehaviour
 {
     private PlayerMovement playerMovement;
+    private PlayerFootSteps playerFootSteps;
     private Transform lookRoot;
     private float sprintSpeed = 8.0f;
     private float moveSpeed = 4.0f;
     private float crouchSpeed = 1.5f;
     private float standHeight = 1.6f;
     private float crouchHeight = 1.0f;
+    private float sprintVolume = 1f;
+    private float crouchVolume = 0.1f;
+    private float walkVolumeMin = 0.2f, walkVolumeMax = 0.6f;
+    private float walkStepDistance = 0.4f;
+    private float sprintStepDistance = 0.25f;
+    private float crouchStepDistance = 0.5f;
     private bool isCrouching;
     private void Awake()
     {
         playerMovement = GetComponent<PlayerMovement>();
+        playerFootSteps = GetComponentInChildren<PlayerFootSteps>();
         lookRoot = transform.GetChild(0);
+    }
+    private void Start()
+    {
+        playerFootSteps.volumeMin = walkVolumeMin;
+        playerFootSteps.volumeMax = walkVolumeMax;
+        playerFootSteps.stepDistance = walkStepDistance;
     }
     private void Update()
     {
@@ -25,10 +39,17 @@ public class PlayerSprintAndCrouch : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching)
         {
             playerMovement.playerSpeed = sprintSpeed;
+            playerFootSteps.stepDistance = sprintStepDistance;
+            playerFootSteps.volumeMin = sprintVolume;
+            playerFootSteps.volumeMax = sprintVolume;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching)
         {
             playerMovement.playerSpeed = moveSpeed;
+
+            playerFootSteps.stepDistance = walkStepDistance;
+            playerFootSteps.volumeMin = walkVolumeMin;
+            playerFootSteps.volumeMax = walkVolumeMax;
         }
     }
     private void Crouch()
@@ -39,12 +60,22 @@ public class PlayerSprintAndCrouch : MonoBehaviour
             {
                 lookRoot.localPosition = new Vector3(0f, standHeight, 0f);
                 playerMovement.playerSpeed = moveSpeed;
+
+                playerFootSteps.stepDistance = walkStepDistance;
+                playerFootSteps.volumeMin = walkVolumeMin;
+                playerFootSteps.volumeMax = walkVolumeMax;
+
                 isCrouching = false;
             }
             else
             {
                 lookRoot.localPosition = new Vector3(0f, crouchHeight, 0f);
+
                 playerMovement.playerSpeed = crouchSpeed;
+                playerFootSteps.stepDistance = crouchStepDistance;
+                playerFootSteps.volumeMin = crouchVolume;
+                playerFootSteps.volumeMax = crouchVolume;
+
                 isCrouching = true;
             }
         }
