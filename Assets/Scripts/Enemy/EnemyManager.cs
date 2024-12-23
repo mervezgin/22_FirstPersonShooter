4459b117c32bde787b5dbe35f91fea2b208f8753
mervezgin <mervezgin@gmail.com>
2024-12-23 10:44:13 +0300

    created EnemyManager, and dragged prefabs

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager instance;
+    [SerializeField] private GameObject boarPrefab, cannibalPrefab;
+    [SerializeField] private Transform[] cannibalSpawnPoints, boarSpawnPoints;
+    [SerializeField] private float waitBeforeSpawnEnemiesTime = 10.0f;
+    [SerializeField] private int cannibalEnemyCount, boarEnemyCount;
+    private int initialCannibalCount, initialBoarCount;
+
     private void Awake()
     {
         MakeInstance();
